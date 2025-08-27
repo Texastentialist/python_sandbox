3803043aeaf4ecf732debe3e9eecfbb5d134d11c
Gerald Parrish <g4244759@gmail.com>
2025-08-27 11:16:37 -0500

    Updated var and str files

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,5 +1,10 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'Gerald'
+age = 57
+
+#Concatenate
+print('Hello my names is ' + name)
 
 # String Formatting
 
